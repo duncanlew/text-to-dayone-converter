caa5c326e14cae8f44da35a914aaf0cc4f9cba08
Duncan Lew <duncanlew@users.noreply.github.com>
2025-06-14 22:24:03 +0200

    example processing of text

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
+import { promises as fsPromises } from 'fs';
+import * as path from "node:path";
+
+async function readFileAsync() {
+    try {
+        const filePath = path.join('assets', '2025-06-14.txt');
+        const content = await fsPromises.readFile(filePath, 'utf-8');
+        console.log(content);
+    } catch (error) {
+        console.error('Error reading file: ', error);
+    }
+}
+
 console.log('hello world');
+readFileAsync();
